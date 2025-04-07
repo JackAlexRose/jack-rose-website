6fd4b7c28a586088bf727d20625e810f6f5b655e
Jack Rose <jackalexanderrose@hotmail.com>
2025-04-07 14:18:44 +0100

    404 page part 2

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
-
+import Image from "next/image";
 const NotFound: FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center gradient-background">
       <div className="text-center p-8">
+        <Image
+          src="/kima.png"
+          alt="404"
+          width={200}
+          height={200}
+          className="w-full h-auto mb-4"
+        />
         <h1 className="text-4xl font-bold text-gray-900 mb-4">
           Nothing to see here yet
         </h1>
